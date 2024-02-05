675dd350beb037ab6fa7a9046a9a8ec0a73f1c92
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2024-02-05 16:08:10 +0800

    footer component

---
+++ src/shared/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  faRightFromBracket = faRightFromBracket;
+}
