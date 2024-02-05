6b365426cdedf4ff67caaaba63b639f649a578b0
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2024-02-05 21:18:37 +0800

    更改靜態檔案路徑

--- src/app/intro-video/intro-video.component.ts
+++ src/app/intro-video/intro-video.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../../shared/menu/menu.component";
 
 @Component({
-  selector: 'app-intro-video',
-  standalone: true,
-  imports: [],
-  templateUrl: './intro-video.component.html',
-  styleUrl: './intro-video.component.scss'
+    selector: 'app-intro-video',
+    standalone: true,
+    templateUrl: './intro-video.component.html',
+    styleUrl: './intro-video.component.scss',
+    imports: [MenuComponent]
 })
 export class IntroVideoComponent {
 
